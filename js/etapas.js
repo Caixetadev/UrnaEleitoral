c44d6a2162d7c7a9c1a5feaa07cc4efa23686ee9
Rafael Caixeta <caixetadev@gmail.com>
2021-08-17 03:54:41 -0300

    First Commit

---
+++ js/etapas.js
@@ -0,0 +1,66 @@
+let etapas = [
+  {
+      titulo: 'VEREADOR',
+      numeros: 5,
+      candidatos: [
+          {
+              numero: '55123',
+              nome: 'Mia Khalifa',
+              partido: 'PORN',
+              fotos: [
+                  {url: 'https://pbs.twimg.com/media/DXN78fAW4AA1Aen?format=jpg', legenda: 'Vereadora'}
+              ]
+          },
+          {
+              numero: '12345',
+              nome: 'Danilo Gentili',
+              partido: 'PRTB',
+              fotos: [
+                  {url: 'https://veja.abril.com.br/wp-content/uploads/2021/07/DANILO-GENTILLI-114.jpg2_.jpg?quality=70&strip=info', legenda: 'Vereador'}
+              ]
+          },
+          {
+              numero: '67890',
+              nome: 'Pica Pau',
+              partido: 'PT',
+              fotos: [
+                  {url: 'https://img.elo7.com.br/product/zoom/28E9B51/matriz-de-bordado-pica-pau-bordadeira.jpg', legenda: 'Vereador'}
+              ]
+          },
+          {
+              numero: '92001',
+              nome: 'Albert Einstein',
+              partido: 'DEFG',
+              fotos: [
+                  {url: 'http://conteudo.imguol.com.br/c/entretenimento/c3/2017/11/24/albert-einstein-1511565360545_v2_1513x1920.jpg', legenda: 'Vereador'}
+              ]
+          }
+      ]
+  },
+  {
+      titulo: 'PRESIDENTE',
+      numeros: 2,
+      candidatos: [
+          {
+              numero: '17',
+              nome: 'Jair Bolsonaro ',
+              partido: 'PRTB',
+              vice: 'Hamilton Mourão',
+              fotos: [
+                  {url: 'https://divulgacandcontas.tse.jus.br/candidaturas/oficial/2018/BR/BR/2022802018/280000614517/foto_1534215592223.jpg', legenda: 'Presidente'},
+                  {url: 'https://divulgacandcontas.tse.jus.br/candidaturas/oficial/2018/BR/BR/2022802018/280000614518/foto_1534215592625.jpg', legenda: 'Vice-Presidente', small: true}
+              ] 
+          },
+          {
+              numero: '13',
+              nome: 'Lula',
+              partido: 'PT',
+              vice: 'José Alencar',
+              fotos: [
+                  {url: 'https://divulgacandcontas.tse.jus.br/candidaturas/oficial/2018/BR/BR/2022802018/280000625869/foto_1534363925445.jpg', legenda: 'Presidente'},
+                  {url: 'https://divulgacandcontas.tse.jus.br/candidaturas/oficial/2018/BR/BR/2022802018/280000629808/foto_1536701262333.jpg', legenda: 'Vice-Presidente', small: true}
+              ] 
+          }
+      ]
+  }
+]
